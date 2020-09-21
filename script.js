0811f90a7f0b4491701c276d9340cb14ead8533e
setdjohn <70076100+setdjohn@users.noreply.github.com>
2020-09-21 11:06:10 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var prompter1 = Number(prompt("NUMBER"));
+
+if(number =5<=prompter1)
+ { var str = "prompt";
+  str.repeat(2);
+}
+else if(number =10>=prompter1){
+  wrapperEle.innerHTML=prompter1;
+}
+else{
+  wrapperEle.innerHTML=" NUMBER";
+var str = "prompt";
+  str.repeat(2);
+}
